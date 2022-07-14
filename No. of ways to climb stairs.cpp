312ad7adb4650a80fcd854c18bd0ebbf1dba3ad6
Rohit Ranjan <83030734+rohit1ranjan1@users.noreply.github.com>
2022-07-15 05:24:45 +0530

    Create No. of ways to climb stairs.cpp

---
+++ No. of ways to climb stairs.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+void calculateWays(int n){
+   
+    if(n==0){
+        cout<<"Number of ways to climb upstairs is: 0";
+        return;
+    }
+    int ways=n/3;
+    ways=pow(3,ways);
+    
+    if(n%3==2)
+    ways=ways*2;
+    
+    cout<<"Number of ways to climb upstairs is: "<<ways;
+}
+int main(){
+    calculateWays(5);
+}
+/*
+
+Number of ways to climb up 5 stairs is: 6
+*/
